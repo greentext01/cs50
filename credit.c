691066d8d149cdb1d228573794a3824ca7086505
greentext01 <greentext01@users.noreply.github.com>
2021-04-17 21:16:49 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -1,74 +1,83 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
-#define MAX 100
+#include <string.h>
 
-int main(void) {
-  long cc_num;
-  short cc_digits[MAX] = {};
-  short cc_other_digits[MAX] = {};
-  short cc_digits_not_mult[MAX] = {};
-  int k;
-  int i = 0;
-  size_t cc_num_size;
-  size_t cc_other_digits_size;
+string getNum(string prompt, int* size) {
+  string out;
   bool is_valid;
-  string cc_type;
-  int multiplied_other_digits;
 
-  cc_num = get_long("Enter your credit card number: ");
+  while (true) {
+    is_valid = true;
+    out = get_string("%s", prompt);
+    *size = strlen(out);
+    for (int i = 0; i < *size; i++) {
+      if (!isdigit(out[i])) {
+        is_valid = false;
+      }
 
-  do {
-    k = cc_num % 10;
-    cc_digits[i] = k;
-    i++;
-    cc_num /= 10;
-  } while (cc_num != 0);
+      out[i] -= 48;
+    }
 
-  cc_num_size = --i;
-  i = 0;
-
-  if (cc_digits[cc_num_size] == 4) {
-    cc_type = "VISA";
-  } else if (cc_digits[cc_num_size] == 3 &&
-             (cc_digits[cc_num_size - 1] == 4 || cc_digits[cc_num_size - 1] == 7)) {
-    cc_type = "AMEX";
-  } else {
-    cc_type = "MASTERCARD";
-  }
-
-  for (int j = cc_num_size; j > 0; j -= 2) {
-    cc_other_digits[i] = cc_digits[j] * 2;
-    i++;
-  }
-  cc_other_digits_size = --i;
-
-  i = 0;
-
-  for (int j = 0; j <= cc_other_digits_size; j++) {
-    if (cc_other_digits[j] >= 10) {
-      i += cc_other_digits[j] % 10;
-      i += 1;
-    } else {
-      i += cc_other_digits[j];
+    if (is_valid) {
+      return out;
     }
   }
+}
 
-  multiplied_other_digits = i;
+bool checkValid(string cc_num, int cc_num_size) {
+  if (cc_num_size > 19) {
+    return 0;
+  }
+  char mult_digits[100];
+  int total = 0;
+  int j = 0;
+  string cc_type;
 
-  i = 0;
-
-  for (int j = cc_num_size - 1; j >= 0; j -= 2) {
-    cc_digits_not_mult[i] = cc_digits[j];
-    i++;
+  for (int i = cc_num_size - 2; i >= 0; i -= 2) {
+    mult_digits[j] = cc_num[i] * 2;
+    j++;
   }
 
-  for (int j = 0; j < i; j++) {
-    multiplied_other_digits += cc_digits_not_mult[j];
+  for (int i = 0; i < j; i++) {
+    if (mult_digits[i] > 9) {
+      total += 1;
+    }
+    total += mult_digits[i] % 10;
   }
 
-  if ((multiplied_other_digits % 10) != 0) {
-    cc_type = "INVALID";
+  for (int i = cc_num_size - 1; i >= 0; i -= 2) {
+    total += cc_num[i];
   }
 
-  printf("%s\n", cc_type);
+  if (total % 10 != 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+bool checkMasterCard(string cc_num) {
+  return cc_num[0] == 5 && (cc_num[1] == 1 || cc_num[1] == 2 ||
+                            cc_num[1] == 3 || cc_num[1] == 4 || cc_num[1] == 5);
+}
+
+string check_cc_type(string cc_num, int cc_num_size) {
+  if (!checkValid(cc_num, cc_num_size)) {
+    return "INVALID";
+  } else if (cc_num[0] == 4) {
+    return "VISA";
+  } else if (cc_num[0] == 3 && (cc_num[1] == 4 || cc_num[1] == 7)) {
+    return "AMEX";
+  } else if (checkMasterCard(cc_num)) {
+    return "MASTERCARD";
+  } else {
+    return "INVALID";
+  }
+}
+
+int main(void) {
+  int size;
+  string cc_num = getNum("Enter your credit card number: ", &size);
+  printf("%s\n", check_cc_type(cc_num, size));
 }
