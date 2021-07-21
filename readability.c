32b91f8678818f7ab12e2add191bc1c2f3e3bf96
greentext01 <greentext01@users.noreply.github.com>
2021-07-21 21:13:19 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -10,9 +10,16 @@ size_t count_sentences(string str);
 
 int main()
 {
+    // Prompt user
     string str = get_string("Text: ");
+
+    // Get letters per 100 words
     double letPer100w = (double)count_letters(str) * 100 / (double)count_words(str);
+
+    // Get sentences per 100 words
     double sentPer100w = (double)count_sentences(str) * 100 / (double)count_words(str);
+
+    // Compute grade
     int grade = round(0.0588 * letPer100w - 0.296 * sentPer100w - 15.8);
     if (grade < 1)
     {
@@ -33,6 +40,7 @@ size_t count_letters(string str)
     size_t count = 0;
     for (size_t i = 0; i < strlen(str); i++)
     {
+        // Adds to the count only if character is alphabetic
         if (isalpha(str[i]))
         {
             count++;
@@ -44,6 +52,7 @@ size_t count_letters(string str)
 
 size_t count_words(string str)
 {
+    // 1 word if string isn't empty
     int wCount = strcmp(str, "") != 0;
     for (size_t i = 0; i < strlen(str); i++)
     {
@@ -61,6 +70,7 @@ size_t count_sentences(string str)
     size_t sCount = 0;
     for (size_t i = 0; i < strlen(str); i++)
     {
+        // Adds to count only if character is punctuaion
         if (str[i] == '.' || str[i] == '?' || str[i] == '!')
         {
             sCount++;
