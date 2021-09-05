32655c36a0a6a9a4fcb5658e5adeae82d4ee0486
greentext01 <greentext01@users.noreply.github.com>
2021-09-05 22:49:07 +0000

    automated commit by check50 [check50=True]

--- application.py
+++ application.py
@@ -70,7 +70,7 @@ def buy():
     if request.method == "POST":
         stock = lookup(request.form.get("symbol"))
         if stock == None:
-            return apology("Symbol does not exist!", 404)
+            return apology("Symbol does not exist!")
 
         price = stock["price"]
 
@@ -81,7 +81,7 @@ def buy():
             return apology('"Shares" is not a number')
 
         if balance[0]["cash"] < price * int(request.form.get("shares")):
-            return apology("Can't afford!", 403)
+            return apology("Can't afford!")
 
         db.execute("UPDATE users SET cash = ? WHERE id = ?",
                    balance[0]["cash"] - price * int(request.form.get("shares")), session["user_id"])
@@ -123,11 +123,11 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?",
@@ -135,7 +135,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -167,7 +167,7 @@ def quote():
     if request.method == "POST":
         resp = lookup(request.form.get("symbol"))
         if resp == None:
-            return apology("Symbol does not exist", 404)
+            return apology("Symbol does not exist")
         return render_template("quoted.html", name=resp["name"], symbol=resp["symbol"], price=usd(resp["price"]))
     else:
         return render_template("quote.html")
@@ -182,13 +182,16 @@ def register():
             "SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         if len(check) != 0:
-            return apology("Username already taken", 409)
+            return apology("Username already taken")
+        
+        if request.form.get("username") == "":
+            return apology("Username is empty")
 
         if request.form.get("password") == "":
-            return apology("Password is empty", 406)
+            return apology("Password is empty")
 
         if request.form.get("password") != request.form.get("confirmation"):
-            return apology("Passwords don't match", 406)
+            return apology("Passwords don't match")
 
         db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get(
             "username"), generate_password_hash(request.form.get("password")))
